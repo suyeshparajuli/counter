35e2b6c7ffa0a89be75b2966bfaab49b4edb5f61
suyesh parajuli <suyeshparajuli11@gmail.com>
2022-12-27 09:46:20 +0545

    counter.js and counter.html added

---
+++ counter.js
@@ -0,0 +1,29 @@
+let count = 0;
+let value = document.getElementById("value");
+let btns = document.querySelectorAll(".btn");
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count --;
+        }
+        else if(styles.contains("increase")){
+            count ++;
+        }else{
+            count = 0;
+        }
+        if(count <0){
+            value.style.color = "red";
+        }else if(count > 0){
+            value.style.color = "green";
+        }else{
+            value.style.color = "blue";
+        }
+        value.textContent = count;
+
+    });
+    
+});
+
+
+
